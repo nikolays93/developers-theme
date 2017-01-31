80be43f17548dc1ed6cc5e63629a49f06e21f9e3
NikolayS93 <nikolays93@ya.ru>
2017-01-31 23:02:37 +0400

    change single product image to css

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -12,7 +12,9 @@ global $post, $product;
 <div class="images">
 	<?php
 		if ( has_post_thumbnail() ) {
-			$props = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
+			$attachment_count = count( $product->get_gallery_attachment_ids() );
+			$gallery          = $attachment_count > 0 ? '[product-gallery]' : '';
+			$props            = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
 			$image_url = get_the_post_thumbnail_url($post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ));
 			$image = sprintf(
 				'<div style="background-image: url(%s);" title="%s" alt="%s"></div>',
@@ -24,9 +26,10 @@ global $post, $product;
 			echo apply_filters(
 				'woocommerce_single_product_image_html',
 				sprintf(
-					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" rel="image-group" data-rel="prettyPhoto[product-gallery]">%s</a>',
+					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" rel="image-group" data-rel="prettyPhoto%s">%s</a>',
 					esc_url( $props['url'] ),
 					esc_attr( $props['caption'] ),
+					$gallery,
 					$image
 				),
 				$post->ID
