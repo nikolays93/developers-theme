9f559418d9c5428872ceed5e1f30f002d57421ce
NikolayS93 <nikolays93@ya.ru>
2017-02-06 21:53:40 +0400

    fix title icon edit

--- include/functions-custom.php
+++ include/functions-custom.php
@@ -1,5 +1,7 @@
 <?php
-// if(class_exists('WPAdvancedPostType')){
-  // $types = new WPAdvancedPostType();
-  // $types->add_type('news', 'Новость', 'Новости');
-// }
+if(class_exists('WPAdvancedPostType')){
+	$types = new WPAdvancedPostType();
+	$types -> add_type( 'enty', 'Entity', 'Entities', array('public'=>false) );
+	$types -> add_type( 'news', 'News');
+	$types -> reg_types();
+}
